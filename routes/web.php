dfcadb52dff70191ad318391b5cadf89c24ddec4
Samir Agudelo Curaca <sagudelo7315@campuscecep.edu.co>
2025-04-05 03:10:30 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas', [ComunaController::class, 'index']);
